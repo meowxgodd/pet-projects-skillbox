0cdc61ecc4807a904f19524c99a50ebb4ca48aa3
meowxgodd <poklonnov212@gmail.com>
2026-08-12 16:03:54 +0300

    feat: попап с подарком при первой покупке (11-dom-попап-подарок)

---
+++ 11-dom-попап-подарок/main.js
@@ -0,0 +1,61 @@
+const giftArr = [
+  {
+    title: "Скидка 20% на первую покупку в нашем магазине!",
+    icon: "/задача 11/1902863.svg"
+  },
+  {
+    title: "Скидка 10% на всё!",
+    icon: "/задача 11/1902863.svg"
+  },
+  {
+    title: "Подарок при первой покупке в нашем магазине!",
+    icon: "/задача 11/1902863.svg"
+  },
+  {
+    title: "Бесплатная доставка для вас!",
+    icon: "/задача 11/1902863.svg"
+  },
+  {
+    title: "Сегодня день больших скидок!",
+    icon: "/задача 11/1902863.svg"
+  }
+]
+
+const body = document.querySelector('body')
+
+const content = document.createElement('div');
+const imgBlock = document.createElement('div');
+imgBlock.classList.add('imgBlock')
+content.append(imgBlock)
+const img = document.createElement('img');
+
+const block = document.createElement('div');
+block.classList.add('block')
+content.append(block)
+const title = document.createElement('h1');
+const btn = document.createElement('button');
+
+
+btn.addEventListener('click', () => {
+  content.remove()
+})
+
+
+
+function renderRandomGift() {
+  body.append(content)
+  content.classList.add('container')
+  const random = Math.floor(Math.random() * giftArr.length)
+  title.textContent = giftArr[random].title
+  block.prepend(title)
+  img.setAttribute('src', `${giftArr[random].icon}`)
+  imgBlock.append(img)
+  btn.textContent = 'Отлично!'
+  block.append(btn)
+
+}
+
+setTimeout(() => {
+  renderRandomGift()
+}, 3000)
+
