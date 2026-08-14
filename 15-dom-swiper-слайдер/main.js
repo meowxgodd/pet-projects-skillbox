08103f95d65de3336bbd2958c41e5f3395cac789
meowxgodd <poklonnov212@gmail.com>
2026-08-14 12:57:09 +0300

    feat: слайдер изображений на Swiper.js (15-dom-swiper-слайдер)

---
+++ 15-dom-swiper-слайдер/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 2,
+	spaceBetween: 20,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+
+});
